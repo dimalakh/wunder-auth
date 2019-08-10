4094f865cf97da9cd56d36ca4af62d8323d929a1
Dmytro Lakh <dmytrolakh@gmail.com>
2019-08-10 16:38:01 +0300

    Used PureComponent instead of component for better efficiency

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class Form extends Component {
+class Form extends PureComponent {
   constructor(props) {
     super(props);
     this.handleChange = this.handleChange.bind(this);
